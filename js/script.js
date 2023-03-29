d79de0b011be078b2ca2cbbe9103ef86a1e24944
Gustavo Berny <gberny@imply.com>
2023-03-29 11:00:46 -0300

    email working

--- js/script.js
+++ js/script.js
@@ -12,3 +12,27 @@ ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact fo
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
+//Send Email
+function sendEmail() {
+    var params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        title: document.getElementById('title').value,
+        message: document.getElementById('message').value,
+    }
+    
+    const serviceID = "service_egrmikc";
+    const templateID = "template_kpp2evs";
+    
+    emailjs.send(serviceID, templateID, params).then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("title").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent siccessfully");
+    })
+    .catch((err) => console.log(err));
+}
